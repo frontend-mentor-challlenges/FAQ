b8767abc2727fb317ed5dd8e23c0f2d7c12e49ec
Jan Nieves Cedeno <jnievescedeno2@gmail.com>
2025-04-12 18:34:01 -0400

    Mobile done

---
+++ main.js
@@ -0,0 +1,14 @@
+const click = document.getElementsByClassName("click");
+const info = document.getElementsByClassName("info");
+
+Array.from(click).forEach((item, index) => {
+  item.addEventListener("click", (event) => {
+    const img = item.querySelector("img");
+    img.src = img.src.includes("icon-plus.svg")
+      ? "./assets/images/icon-minus.svg"
+      : "./assets/images/icon-plus.svg";
+
+    info[index].classList.toggle("active");
+    console.log("Index in HTMLCollection:", index);
+  });
+});
